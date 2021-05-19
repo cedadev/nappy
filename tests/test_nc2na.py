6872faf6e4505c205539092e513695c9d9fc19d8
Ag Stephens <ag.stephens@stfc.ac.uk>
2021-05-19 15:02:57 +0000

    Added tests/test_nc2csv.py

--- tests/test_nc2na.py
+++ tests/test_nc2na.py
@@ -4,33 +4,37 @@ import xarray as xr
 
 from nappy.nappy_api import openNAFile
 from nappy.nc_interface.nc_to_na import NCToNA
+import nappy.utils
 
 from .common import data_files, test_outputs, cached_outputs
 
+DELIMITER = nappy.utils.getDefault("default_delimiter") 
+EXTENSION = "na"
 
-def _get_paths(ffi, label=""):
+
+def _get_paths(ffi, label="", extension=EXTENSION):
     infile = os.path.join(cached_outputs, f"{ffi}.nc")
 
     if label:
-        outfilename = f"{ffi}-{label}.nc.na"
+        outfilename = f"{ffi}-{label}.nc.{extension}"
     else:
-        outfilename = f"{ffi}.nc.na"
+        outfilename = f"{ffi}.nc.{extension}"
 
     outfile = os.path.join(test_outputs, outfilename)
 
     return infile, outfile
     
 
-def _generic_nc_to_na_test(ffi):
+def _generic_nc_to_na_test(ffi, delimiter=DELIMITER, extension=EXTENSION):
     """
     Command-line equivalent:
       $ nc2na.py -i test_outputs/${ffi}.nc -o test_outputs/${ffi}-from-nc.na
 
     """
-    infile, outfile = _get_paths(ffi)
+    infile, outfile = _get_paths(ffi, extension=extension)
 
     na = NCToNA(infile)
-    na.writeNAFiles(outfile, float_format="%g")
+    na.writeNAFiles(outfile, float_format="%g", delimiter=delimiter)
 
     assert os.path.getsize(outfile) > 100
 
@@ -63,12 +67,12 @@ def test_nc_to_na_1001_exclude():
 
     assert "ascent_rate" not in [v.name for v in na.xr_variables]
 
-    na.writeNAFiles(outfile, float_format="%g")
+    na.writeNAFiles(outfile, float_format="%g", delimiter=DELIMITER)
 
     assert os.path.getsize(outfile) > 100
 
-    ds = xr.open_dataset(outfile)
-    assert 'ascent_rate' not in ds.keys()
+    n = openNAFile(outfile)
+    assert not any([vname.lower().startswith('ascent') for vname in n["VNAME"]])
 
 
 def test_nc_to_na_1001_overwrite_metadata():
@@ -76,7 +80,7 @@ def test_nc_to_na_1001_overwrite_metadata():
     infile, outfile = _get_paths(1001, label="overwrite-metadata")
 
     na = NCToNA(infile, na_items_to_override={"ONAME": "See line 2 - my favourite org."})
-    na.writeNAFiles(outfile, float_format="%g")
+    na.writeNAFiles(outfile, float_format="%g", delimiter=DELIMITER)
 
     n = openNAFile(outfile)
     assert n["ONAME"] == "See line 2 - my favourite org."
