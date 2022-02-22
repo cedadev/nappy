b4553c2a44def5afbffbcce4bd8619642142e075
Florian Obersteiner <florian.obersteiner@kit.edu>
2022-02-22 14:05:50 +0100

    added test for custom regex setter

--- tests/test_var_units.py
+++ tests/test_var_units.py
@@ -9,9 +9,13 @@ import os
 import re
 import pytest
 
-from .common import data_files
 import nappy
 
+from .common import data_files
+
+
+
+infile = os.path.join(data_files, "2010b.na")
 
 def custom_parser(string):
     """Custom parser for separating variable name from units
@@ -40,9 +44,23 @@ expected2[2] = ('Potential vorticity', 'K m**2/(kg s)')
     "parser,expected",
     [(None, expected1),
      (custom_parser, expected2)])
-def test_var_units(parser, expected):
-
-    infile = os.path.join(data_files, "2010b.na")
+def test_var_units_callback(parser, expected):
+    """test the callback function for parsing VNAMEs"""
     fin = nappy.openNAFile(infile, var_and_units_callback=parser)
-    vars = [(v[0], v[1]) for v in fin.getVariables()]
-    assert vars == expected
+    variables = [(v[0], v[1]) for v in fin.getVariables()]
+    assert variables == expected
+
+
+def test_var_units_pattern():
+    """test the possibility to set the regex that is used by default to parse VNAMEs"""
+    fin = nappy.openNAFile(infile)
+    default_re = re.compile(r"^\s*(.*)\((.+?)\)(.*)\s*$")
+    assert fin.var_and_units_pattern == default_re
+    new_re = re.compile(r'(?P<name>.*)\;\ (?P<description>.*)\;\ (?P<unit>.*)', re.UNICODE)
+    fin.var_and_units_pattern = new_re
+    assert fin.var_and_units_pattern == new_re
+
+    invalid = ("a string", 555, 3.142, lambda x: x*2)
+    for i in invalid:
+        with pytest.raises(TypeError):
+            fin.var_and_units_pattern = i
